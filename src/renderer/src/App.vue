1b3344dab9e68ea0d52fc8f6e660e9e1aee86bac
Juan <juanpanizzi@hotmail.com>
2025-03-16 19:05:52 -0300

    icons and menubar

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,26 +1,25 @@
 <script setup lang="ts">
+import { Menubar } from 'primevue';
+import { ref } from 'vue';
+
 
 const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+const items = ref([
+    {
+        label: 'Home',
+        icon: 'pi pi-home'
+    },
+    {
+        label: 'Features',
+        icon: 'pi pi-star'
+    },
+  ])
 </script>
 
 <template>
-  <!-- <img alt="logo" class="logo" src="./assets/electron.svg" />
-  <div class="creator">Powered by electron-vite</div>
-  <div class="text">
-    Build an Electron app with
-    <span class="vue">Vue</span>
-    and
-    <span class="ts">TypeScript</span>
-  </div>
-  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
-  <div class="actions">
-    <div class="action">
-      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
+ <div class="card">
+        <Menubar :model="items" />
     </div>
-    <div class="action">
-      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
-    </div>
-  </div>
-  <Versions /> -->
-  <h1>hola</h1>
+    <h1>hola</h1>
 </template>
