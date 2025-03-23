11a2e43f71a1a5e985f06d94cc4aa3a93661a71d
Juan <juanpanizzi@hotmail.com>
2025-03-22 23:44:50 -0300

    datatable product advances

--- src/renderer/src/components/Home.vue
+++ src/renderer/src/components/Home.vue
@@ -1,6 +1,13 @@
-<script setup>
-</script>
 <template>
 
     <h1>Home Component</h1>
+    
+    <ProductsTable />
+
+
 </template>
+<script setup>
+
+import ProductsTable from './ProductsTable.vue';
+
+</script>
