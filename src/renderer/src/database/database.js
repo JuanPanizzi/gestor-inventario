7c817089f32c2d73ec7872b48395794a7178c902
Juan <juanpanizzi@hotmail.com>
2025-03-23 01:13:20 -0300

    adv db

--- src/renderer/src/database/database.js
+++ src/renderer/src/database/database.js
@@ -4,4 +4,24 @@ const dbPath = '../database/database.db'
 
 const db = new sqlite3(dbPath);
 
+
+const addProductsSQL = `CREATE TABLE IF NOT EXISTS products (
+id INTEGER PRIMARY KEY,
+name TEXT,
+brand TEXT,
+provider TEXT,
+sale_price INTEGER,
+cost_price INTEGER,
+stock INTEGER,
+category TEXT,
+description TEXT,
+status TEXT,
+notes TEXT,
+created_at TEXT,
+updated_at TEXT,
+location TEXT
+)`
+
+
+
 export default db;
