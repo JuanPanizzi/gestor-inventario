1b0a3aba50d43445827994c8ebba77fcc5215cf4
Juan <juanpanizzi@hotmail.com>
2025-03-22 23:48:50 -0300

    adv products table

--- src/renderer/src/components/ProductsTable.vue
+++ src/renderer/src/components/ProductsTable.vue
@@ -1,8 +1,7 @@
-
 <template>
     <div class="card">
-        <DataTable v-model:filters="filters" :value="customers" paginator :rows="10" dataKey="id" filterDisplay="row" :loading="loading"
-                :globalFilterFields="['name', 'country.name', 'representative.name', 'status']">
+        <DataTable v-model:filters="filters" :value="customers" paginator :rows="10" dataKey="id" filterDisplay="row"
+            :loading="loading" :globalFilterFields="['name', 'country.name', 'representative.name', 'status']">
             <template #header>
                 <div class="flex justify-end">
                     <IconField>
@@ -20,32 +19,40 @@
                     {{ data.name }}
                 </template>
                 <template #filter="{ filterModel, filterCallback }">
-                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()" placeholder="Search by name" />
+                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()"
+                        placeholder="Search by name" />
                 </template>
             </Column>
             <Column header="Country" filterField="country.name" style="min-width: 12rem">
                 <template #body="{ data }">
                     <div class="flex items-center gap-2">
-                        <img alt="flag" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="`flag flag-${data.country.code}`" style="width: 24px" />
+                        <img alt="flag" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+                            :class="`flag flag-${data.country.code}`" style="width: 24px" />
                         <span>{{ data.country.name }}</span>
                     </div>
                 </template>
                 <template #filter="{ filterModel, filterCallback }">
-                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()" placeholder="Search by country" />
+                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()"
+                        placeholder="Search by country" />
                 </template>
             </Column>
             <Column header="Agent" filterField="representative" :showFilterMenu="false" style="min-width: 14rem">
                 <template #body="{ data }">
                     <div class="flex items-center gap-2">
-                        <img :alt="data.representative.name" :src="`https://primefaces.org/cdn/primevue/images/avatar/${data.representative.image}`" style="width: 32px" />
+                        <img :alt="data.representative.name"
+                            :src="`https://primefaces.org/cdn/primevue/images/avatar/${data.representative.image}`"
+                            style="width: 32px" />
                         <span>{{ data.representative.name }}</span>
                     </div>
                 </template>
                 <template #filter="{ filterModel, filterCallback }">
-                    <MultiSelect v-model="filterModel.value" @change="filterCallback()" :options="representatives" optionLabel="name" placeholder="Any" style="min-width: 14rem" :maxSelectedLabels="1">
+                    <MultiSelect v-model="filterModel.value" @change="filterCallback()" :options="representatives"
+                        optionLabel="name" placeholder="Any" style="min-width: 14rem" :maxSelectedLabels="1">
                         <template #option="slotProps">
                             <div class="flex items-center gap-2">
-                                <img :alt="slotProps.option.name" :src="`https://primefaces.org/cdn/primevue/images/avatar/${slotProps.option.image}`" style="width: 32px" />
+                                <img :alt="slotProps.option.name"
+                                    :src="`https://primefaces.org/cdn/primevue/images/avatar/${slotProps.option.image}`"
+                                    style="width: 32px" />
                                 <span>{{ slotProps.option.name }}</span>
                             </div>
                         </template>
@@ -57,7 +64,8 @@
                     <Tag :value="data.status" :severity="getSeverity(data.status)" />
                 </template>
                 <template #filter="{ filterModel, filterCallback }">
-                    <Select v-model="filterModel.value" @change="filterCallback()" :options="statuses" placeholder="Select One" style="min-width: 12rem" :showClear="true">
+                    <Select v-model="filterModel.value" @change="filterCallback()" :options="statuses"
+                        placeholder="Select One" style="min-width: 12rem" :showClear="true">
                         <template #option="slotProps">
                             <Tag :value="slotProps.option" :severity="getSeverity(slotProps.option)" />
                         </template>
@@ -66,10 +74,12 @@
             </Column>
             <Column field="verified" header="Verified" dataType="boolean" style="min-width: 6rem">
                 <template #body="{ data }">
-                    <i class="pi" :class="{ 'pi-check-circle text-green-500': data.verified, 'pi-times-circle text-red-400': !data.verified }"></i>
+                    <i class="pi"
+                        :class="{ 'pi-check-circle text-green-500': data.verified, 'pi-times-circle text-red-400': !data.verified }"></i>
                 </template>
                 <template #filter="{ filterModel, filterCallback }">
-                    <Checkbox v-model="filterModel.value" :indeterminate="filterModel.value === null" binary @change="filterCallback()" />
+                    <Checkbox v-model="filterModel.value" :indeterminate="filterModel.value === null" binary
+                        @change="filterCallback()" />
                 </template>
             </Column>
         </DataTable>
@@ -79,7 +89,7 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { FilterMatchMode } from '@primevue/core/api';
-import { CustomerService } from '@/service/CustomerService';
+import { Column, DataTable, IconField, InputIcon, InputText } from 'primevue';
 
 const customers = ref();
 const filters = ref({
@@ -106,10 +116,28 @@ const statuses = ref(['unqualified', 'qualified', 'new', 'negotiation', 'renewal
 const loading = ref(true);
 
 onMounted(() => {
-    CustomerService.getCustomersMedium().then((data) => {
-            customers.value = getCustomers(data);
-            loading.value = false;
-        });
+
+    customers.value = [
+        {
+            id: 1000,
+            name: 'James Butt',
+            country: {
+                name: 'Algeria',
+                code: 'dz'
+            },
+            company: 'Benton, John B Jr',
+            date: '2015-09-13',
+            status: 'unqualified',
+            verified: true,
+            activity: 17,
+            representative: {
+                name: 'Ioni Bowcher',
+                image: 'ionibowcher.png'
+            },
+            balance: 70663
+        }];
+
+    loading.value = false;
 });
 
 const getCustomers = (data) => {
