aeb3c1af0c15a5eb715eb0f97a90eaee2ac9cdce
Juan <juanpanizzi@hotmail.com>
2025-04-06 18:14:28 -0300

    buttons

--- src/renderer/src/components/ProductsTable.vue
+++ src/renderer/src/components/ProductsTable.vue
@@ -6,13 +6,24 @@
             <Column field="brand" header="Marca" style="width: 25%"></Column>
             <Column field="sale_price" header="Precio de venta" style="width: 25%"></Column>
             <Column field="notes" header="Notas" style="width: 25%"></Column>
+            <Column>
+            <template #body="slotProps">
+                
+                <div class="flex items-center gap-x-2 bg-red-200" style="display: flex;">
+
+                    <Button icon="pi pi-pencil" class="mr-2" severity="success" outlined @click="editProduct(slotProps.data)"></Button>
+                    <Button  icon="pi pi-trash" severity="danger" outlined @click="deleteProduct(slotProps.data)"></Button>
+                </div>
+            </template>
+            </Column>
+            
         </DataTable>
     </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import { Column, DataTable } from 'primevue';
+import { Column, DataTable, Button } from 'primevue';
 import { useProducts } from '../composables/useProducts.js'
 
     const products = ref(null);
